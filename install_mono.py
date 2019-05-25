b8092e15d967743013b9bfb7097a0e196434a26f
cclauss <cclauss@me.com>
2019-05-25 10:27:43 +0200

    print("\n".join(sorted(path1)))

--- install_mono.py
+++ install_mono.py
@@ -6,5 +6,5 @@ from subprocess import check_call
 path0 = set(os.getenv("PATH").split(";"))
 check_call("choco install mono".split())
 path1 = set(os.getenv("PATH").split(";"))
-print(path1)
+print("\n".join(sorted(path1)))
 print("sd:", path0 ^ path1)
