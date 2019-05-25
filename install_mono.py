6c78cdaed9182fb1d2b423388ddb79ff9c0e6766
cclauss <cclauss@me.com>
2019-05-25 09:11:21 +0200

    check_call("choco install mono".split())

--- install_mono.py
+++ install_mono.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import os
-from subprocess import run
+from subprocess import check_call
 
 path0 = set(os.getenv("PATH").split(";"))
-run("choco install mono".split())
+check_call("choco install mono".split())
 path1 = set(os.getenv("PATH").split(";"))
 print(path1)
